ce8e01a0b73f105ca378b13e91c7ce78d05f3062
aotyurin <std-t@bk.ru>
2017-09-10 22:08:11 +0300

    поправил TrendService

--- src/ru/ex4/apibt/service/TrendService.java
+++ src/ru/ex4/apibt/service/TrendService.java
@@ -3,39 +3,44 @@ package ru.ex4.apibt.service;
 import ru.ex4.apibt.IExConst;
 import ru.ex4.apibt.dto.TickerDto;
 import ru.ex4.apibt.dto.TrendType;
-import ru.ex4.apibt.factory.ExFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrendService{
-    private static ExFactory exFactory = ExFactory.exFactoryInstance();
-
-
-    public static TrendType getTrendType() throws IOException {
+public class TrendService {
+    public static TrendType getTrendType(String pair) throws IOException {
+        System.out.println("определяем тренд в течении >= " + IExConst.LAST_TRADE_PERIOD + " мин.");
         List<Float> lastTradeList = new ArrayList<>();
-        fillTradeList(exFactory, lastTradeList);
+        fillTradeList(lastTradeList, pair);
 
         return extractType(lastTradeList);
     }
 
 
-    private static void fillTradeList(ExFactory exFactory, List<Float> lastTradeList) throws IOException {
-        float iter = 5;
-        float pauseMin = IExConst.LAST_TRADE_PERIOD / iter;
+    private static void fillTradeList(List<Float> lastTradeList, String pair) throws IOException {
+        int iter = 5;
+        double pauseMin = (double) IExConst.LAST_TRADE_PERIOD / iter;
 
         for (int i = 0; i < iter; i++) {
-            List<TickerDto> ticker = exFactory.getTicker();
+            TickerDto tickerDto = TickerService.getTickerDtoByPair(pair);
+            if (tickerDto != null) {
+                System.out.println("собираем последнии уник. продажи по lastTrade - " + tickerDto);
 
-            for (TickerDto tickerDto : ticker) {
-                if (tickerDto.getCurrency().equals(IExConst.PAIR)) {
-                    lastTradeList.add(tickerDto.getLastTrade());
+                float lastTradeRound = Math.round(tickerDto.getLastTrade());
+                if (lastTradeList.size() > 0) {
+                    Float aFloat = lastTradeList.get(lastTradeList.size() - 1);
+                    if (lastTradeRound != aFloat) {
+                        lastTradeList.add(lastTradeRound);
+                    } else {
+                        --i;
+                    }
+                } else {
+                    lastTradeList.add(lastTradeRound);
                 }
             }
-
             try {
-                Thread.sleep((long)(1000 * 60 * pauseMin));
+                Thread.sleep((long) (1000 * 60 * pauseMin));
             } catch (InterruptedException ignore) {
             }
         }
@@ -48,7 +53,7 @@ public class TrendService{
             float lastValue = lastTradeList.get(0);
 
             for (int i = 1; i < lastTradeList.size(); i++) {
-                float d = lastValue - lastTradeList.get(i);
+                float d = Math.round(lastValue) - Math.round(lastTradeList.get(i));
                 lastValue = lastTradeList.get(i);
 
                 if ((trendType == TrendType.upward && d > 0) || (trendType == TrendType.downward && d < 0)) {
