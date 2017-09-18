5417fda2ff809f186be68e63f902375b9dba4030
aotyurin <std-t@bk.ru>
2017-09-18 21:32:39 +0300

    логирование

---
+++ src/ru/ex4/apibt/log/Logs.java
@@ -0,0 +1,25 @@
+package ru.ex4.apibt.log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logs {
+    private static String DATE_FORMAT = "yyyy-MM-dd HH:mm";
+
+    public static void info(String message) {
+
+        Date date = new Date();
+        String str = new SimpleDateFormat(DATE_FORMAT).format(date) + "  \t INFO  - [ \t\t " + message + " ]";
+
+        System.out.println(str);
+    }
+
+    public static void error(String message) {
+        Date date = new Date();
+        String str = new SimpleDateFormat(DATE_FORMAT).format(date) + "  \t ERROR  - [ \t\t " + message + " ]";
+
+        System.err.println(str);
+    }
+
+
+}
--- src/ru/ex4/apibt/service/TrendService.java
+++ src/ru/ex4/apibt/service/TrendService.java
@@ -3,6 +3,7 @@ package ru.ex4.apibt.service;
 import ru.ex4.apibt.IExConst;
 import ru.ex4.apibt.dto.TickerDto;
 import ru.ex4.apibt.dto.TrendType;
+import ru.ex4.apibt.log.Logs;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class TrendService {
     public static TrendType getTrendType(String pair) throws IOException {
-        System.out.println("определяем тренд в течении >= " + IExConst.LAST_TRADE_PERIOD + " мин.");
+        Logs.info("определяем тренд в течении >= " + IExConst.LAST_TRADE_PERIOD + " мин.");
         List<Float> lastTradeList = new ArrayList<>();
         fillTradeList(lastTradeList, pair);
 
@@ -25,7 +26,7 @@ public class TrendService {
         for (int i = 0; i < iter; i++) {
             TickerDto tickerDto = TickerService.getTickerDtoByPair(pair);
             if (tickerDto != null) {
-                System.out.println("собираем последнии уник. продажи по lastTrade - " + tickerDto);
+                Logs.info("собираем последнии уник. продажи по lastTrade - " + tickerDto);
 
                 float lastTradeRound = Math.round(tickerDto.getLastTrade());
                 if (lastTradeList.size() > 0) {
